ce7ae5e4240078b2a64589971a76a374f80965db
yesicablum <yesica.blum@uniminuto.edu.co>
2025-10-14 17:06:42 -0500

    link developer

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api", // ⚙️ cambia si tu backend usa otro puerto o ruta
+  baseURL: "https://backend-molino.onrender.com", // ⚙️ cambia si tu backend usa otro puerto o ruta
 });
 
 // ====================== EMPLEADOS ======================
